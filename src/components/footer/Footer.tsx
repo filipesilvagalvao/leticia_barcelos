e7681ffbb449a4e0023c7364e4951ee5b63eac4d
filipe <felepesilvagalvao@gmail.com>
2026-03-04 15:20:09 -0300

    ajuste na responsividade do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styles from "./Footer.module.css"
 
 function Footer() {
+    
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__container}>
